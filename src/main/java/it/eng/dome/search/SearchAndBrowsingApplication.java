c5365353625662913317006d09931197d2312d4a
pasquy <pasquale.vitale@gmail.com>
2024-05-24 13:52:01 +0200

    Restore no test

--- src/main/java/it/eng/dome/search/SearchAndBrowsingApplication.java
+++ src/main/java/it/eng/dome/search/SearchAndBrowsingApplication.java
@@ -1,43 +1,15 @@
 package it.eng.dome.search;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import it.eng.dome.search.domain.IndexingObject;
-import it.eng.dome.search.service.OfferingProcessor;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class SearchAndBrowsingApplication implements CommandLineRunner {
+public class SearchAndBrowsingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchAndBrowsingApplication.class, args);
 	}
 
-	@Autowired
-	private OfferingProcessor offering;
-
-	@Override
-	public void run(String... args) throws Exception {
-		
-		IndexingObject index = new IndexingObject();
-		index.setId("index-id1");
-		index.setProductOfferingId("product-offering-id1");
-		index.setProductOfferingDescription("product offering description");
-		index.setProductOffering(null);
-		index.setProductOfferingIsBundle(false);
-		index.setProductOfferingLastUpdate("2024-05-21T10:02:40.254833831Z");
-		index.setProductOfferingName("product-offering-name");
-		index.setProductOfferingNameText("Name Text");
-		index.setProductSpecification(null);
-		index.setProductSpecificationBrand("");
-		index.setProductSpecificationDescription("Product Specification Description");
-		index.setProductSpecificationId("product-specification-id1");
-		index.setProductSpecificationName("Specification Name");
-		index.setServices(null);
-		index.setResources(null);
-		offering.save(index);
-	}
 }
