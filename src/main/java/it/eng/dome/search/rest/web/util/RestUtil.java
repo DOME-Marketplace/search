1486ad26136ed21a1540bef13353972d4c8ff02d
pasquy <pasquale.vitale@gmail.com>
2024-06-24 15:18:07 +0200

    Update URL logs

--- src/main/java/it/eng/dome/search/rest/web/util/RestUtil.java
+++ src/main/java/it/eng/dome/search/rest/web/util/RestUtil.java
@@ -1,6 +1,5 @@
 package it.eng.dome.search.rest.web.util;
 
-import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,58 +16,22 @@ public class RestUtil {
 
 	@Value("${dome.tmforum.url}")
 	private String getTMForumUrl;
-	/*
-	 * @Value("${dome.tmforum.port}")
-	 * private String getTMForumPort;
-	 * 
-	 * @Value("${dome.tmforum.path}")
-	 * private String getTMForumPath;
-	 * 
-	 * @Value("${dome.bae.schema}")
-	 * private String getBAESchema;
-	 * 
-	 * @Value("${dome.bae.host}")
-	 * private String getBAEHost;
-	 * 
-	 * @Value("${dome.bae.port}")
-	 * private String getBAEPort;
-	 * 
-	 * @Value("${dome.bae.path}")
-	 * private String getBAEPath;
-	 */
+
 	@Value("${dome.bae.url}")
 	private String getBAEUrl;
 
 	private static final Logger log = LoggerFactory.getLogger(RestUtil.class);
 	private static RestTemplate restTemplate = new RestTemplate();
 
-	private String tmForumURL = null;
-	private String baeURL = null;
-
-	private String getTMForumURL() {
-		if (tmForumURL == null) {
-			tmForumURL = getTMForumUrl;
-			log.info("Set TMForum URL {}", tmForumURL);
-		}
-		return tmForumURL;
-	}
-
-	private String getBaeURL() {
-		if (baeURL == null) {
-			baeURL = getBAEUrl;
-			log.info("Set BAE URL {}", baeURL);
-		}
-		return getBAEUrl;
-	}
-
 	/*****************
 	 * BAE Endpoints *
 	 *****************/
 
 	public String getProductOfferingById(String id) {
-		log.debug("Call getProductOfferingById with id {}", id);
-		ResponseEntity<String> response = restTemplate.getForEntity(getBaeURL() + "/catalog/productOffering/" + id,
-				String.class);
+		String url = getBAEUrl + "/catalog/productOffering/" + id;
+		log.debug("Call getProductOfferingById to URL {}", url);
+
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
 		String result = response.getBody();
 		log.debug("Response for getProductOfferingById with status code {}", response.getStatusCode().name());
@@ -76,10 +39,10 @@ public class RestUtil {
 	}
 
 	public String getProductSpecificationById(String id) {
-		log.debug("Call getProductSpecificationById with id {}", id);
+		String url = getBAEUrl + "/catalog/productSpecification/" + id;
+		log.debug("Call getProductSpecificationById to URL {}", url);
 
-		ResponseEntity<String> response = restTemplate
-				.getForEntity(getBaeURL() + "/catalog/productSpecification/" + id, String.class);
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
 		String result = response.getBody();
 		log.debug("Response for getProductSpecificationById with status code {}", response.getStatusCode().name());
@@ -87,9 +50,9 @@ public class RestUtil {
 	}
 
 	public String getServiceSpecificationById(String id) {
-		log.debug("Call getServiceSpecificationById with id {}", id);
-		ResponseEntity<String> response = restTemplate.getForEntity(getBaeURL() + "/service/serviceSpecification/" + id,
-				String.class);
+		String url = getBAEUrl + "/service/serviceSpecification/" + id;
+		log.debug("Call getServiceSpecificationById to URL {}", url);
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
 		String result = response.getBody();
 		log.debug("Response for getServiceSpecificationById with status code {}", response.getStatusCode().name());
@@ -97,19 +60,19 @@ public class RestUtil {
 	}
 
 	public String getResourceSpecificationById(String id) {
-		log.debug("Call getResourceSpecificationById with id {}", id);
-		ResponseEntity<String> response = restTemplate
-				.getForEntity(getBaeURL() + "/resource/resourceSpecification/" + id, String.class);
+		String url = getBAEUrl + "/resource/resourceSpecification/" + id;
+		log.debug("Call getResourceSpecificationById to URL {}", url);
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
 		String result = response.getBody();
 		log.debug("Response for getResourceSpecificationById with status code {}", response.getStatusCode().name());
 		return result;
 	}
 
-	public String getAllProductOfferings() {
-		log.debug("Call getAllProductOfferings");
-		ResponseEntity<String> response = restTemplate.getForEntity(getBaeURL() + "/catalog/productOffering",
-				String.class);
+	public String getAllProductOfferings() {		
+		String url = getBAEUrl + "/catalog/productOffering";
+		log.debug("Call getAllProductOfferings to URL {}", url); 
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
 		String result = response.getBody();
 		log.debug("Response for getAllProductOfferings with status code {}", response.getStatusCode().name());
@@ -121,9 +84,9 @@ public class RestUtil {
 	 *********************/
 
 	public String getTMFProductSpecificationById(String id) {
-		log.debug("Call getTMFProductSpecificationById with id {}", id);
-		ResponseEntity<String> response = restTemplate.getForEntity(
-				getTMForumURL() + "/productCatalogManagement/v4/productSpecification/" + id, String.class);
+		String url = getTMForumUrl + "/productCatalogManagement/v4/productSpecification/" + id;
+		log.debug("Call getTMFProductSpecificationById to URL {}", url);
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
 		String result = response.getBody();
 		log.debug("Response for getTMFProductSpecificationById with status code {}", response.getStatusCode().name());
@@ -131,19 +94,20 @@ public class RestUtil {
 	}
 
 	public String getAllProductOfferingsFromTMForum() {
-		log.debug("Call getAllProductOfferingsFromTMForum");
-		ResponseEntity<String> response = restTemplate
-				.getForEntity(getTMForumURL() + "/serviceCatalogManagement/v4/serviceSpecification", String.class);
+		String url = getTMForumUrl + "/serviceCatalogManagement/v4/serviceSpecification";
+		log.debug("Call getAllProductOfferingsFromTMForum to URL {}", url);
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
 		String result = response.getBody();
-		log.debug("Response for getAllProductOfferingsFromTMForum with status code {}", response.getStatusCode().name());
+		log.debug("Response for getAllProductOfferingsFromTMForum with status code {}",
+				response.getStatusCode().name());
 		return result;
 	}
 
 	public String getTMFProductOfferingById(String id) {
-		log.debug("Call getTMFProductOfferingById with id {}", id);
-		ResponseEntity<String> response = restTemplate
-				.getForEntity(getTMForumURL() + "/productCatalogManagement/v4/productOffering/" + id, String.class);
+		String url = getTMForumUrl + "/productCatalogManagement/v4/productOffering/" + id;
+		log.debug("Call getTMFProductOfferingById to URL {}", url);
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
 		String result = response.getBody();
 		log.debug("Response for getTMFProductOfferingById with status code {}", response.getStatusCode().name());
@@ -151,9 +115,9 @@ public class RestUtil {
 	}
 
 	public String getTMFServiceSpecificationById(String id) {
-		log.debug("Call getTMFServiceSpecificationById with id {}", id);
-		ResponseEntity<String> response = restTemplate
-				.getForEntity(getTMForumURL() + "/productCatalogManagement/v4/productOffering/" + id, String.class);
+		String url = getTMForumUrl + "/productCatalogManagement/v4/productOffering/" + id;
+		log.debug("Call getTMFServiceSpecificationById to URL {}", url);
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
 		String result = response.getBody();
 		log.debug("Response for getTMFServiceSpecificationById with status code {}", response.getStatusCode().name());
@@ -161,9 +125,9 @@ public class RestUtil {
 	}
 
 	public String getTMFResourceSpecificationById(String id) {
-		log.debug("Call getTMFServiceSpecificationById with id {}", id);
-		ResponseEntity<String> response = restTemplate
-				.getForEntity(getTMForumURL() + "/resourceCatalog/v4/resourceSpecification/" + id, String.class);
+		String url = getTMForumUrl + "/resourceCatalog/v4/resourceSpecification/" + id;
+		log.debug("Call getTMFServiceSpecificationById to URL {}", url);
+		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
 		String result = response.getBody();
 		log.debug("Response for getTMFResourceSpecificationById with status code {}", response.getStatusCode().name());
